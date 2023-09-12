8d76df799c5d5e4fa50a97d2fbfc9b225496fb9a
Priscilla Otibo <prisynicholsin@gmail.com>
2023-09-12 08:20:11 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+
+		_putchar(k);
+	_putchar('\n');
+}
