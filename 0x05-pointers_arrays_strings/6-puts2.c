b2d8c445f2b8b7ef3249192bc695ad8976163d6a
Priscilla Otibo <prisynicholsin@gmail.com>
2023-09-19 15:01:55 -0700

    Print every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - function prints every other string
+ * @str: parameter of function
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
