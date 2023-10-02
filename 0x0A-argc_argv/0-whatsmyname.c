f6215c39bf3d746341d3d8e8e81f6c646be7fecc
Priscilla Otibo <prisynicholsin@gmail.com>
2023-10-02 08:24:51 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function prints name of program
+ * @argc: parameter of function
+ * @argv: parameter of function
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+int main(int _attribute_((_unused_)) argc, char *argv[]);
+#endif
